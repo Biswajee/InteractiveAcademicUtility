157a7e54c80365d4d8c7bb944ba9814ca8462435
Biswajee <roy.biswajeet161@gmail.com>
2020-03-26 01:18:30 +0530

    Sanitized my hands and finalized project

---
+++ readme.md
@@ -0,0 +1,116 @@
+# Interactive Academic Utility for COVID 19 Solution Challenge
+
+The repository contains a gesture driven interactive quiz system with grading service and a news reader service. 
+The application can be extensively used as a tool to teach students using only voice and gestures without interacting with any surface. The software 
+can also be deployed to monitor quarantined patients in hospitals and to obtain health surveys and the software can even read out most recent news for patients. 
+Since, the survey questions, results are stored in cloud, there's no need to manually retrieve survey data from the deployed system or update the survey questions.   
+
+## Prerequisites
+
+The InteractiveAcademicUtility Application uses :
++ Maven Java (for dependencies and application)
++ Python (for image processing)
+  + OpenCV (image processing library)
++ Android (for implementing some features like Gson, which isn't available on Maven)
++ IBM Cognitive Services (Watson: text-to-speech SDK)
++ Firebase services used :
+  + Firebase Realtime Database (storing test-scores of users)
+  + Firebase Admin SDK
+  + Firebase Auth (Authenticating into the client platform)
+
+
+## Installation and running
+
+First, **clone** the repository using command :
+`git clone https://github.com/Biswajee/InteractiveAcademicUtility.git`
+
+Make sure you are running **JAVA 8** on your system. If not, head over to install the right one for yourself [here](https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html).
+
+Install **Android SDK** in your system. This will be required since the application uses certain functionalities available with _android java_ but not in _maven java_.
+
+If you are done, you need to have **python 3.6** installed now and set as your default python program in your environment variable of your system.
+
+Wait, there is still a few more things to do with your newly installed python:
+
++ Make sure you have pip installed along with python
++ Install the following python libraries using `pip`
+  + cv2
+  + pyaudio [not implemented]
+  + numpy
+  + copy
+
+Once you have them installed, perform this:
+`pip install --upgrade "watson-developer-cloud>=2.4.1"`
+
+For more information on watson-developer-cloud sdk, head [here](https://github.com/watson-developer-cloud/python-sdk).
+
+
+At this time, running the sample `InteractiveAcademicUtility.jar` supplied in the repository should work without errors.
+
+Incase, you notice erroneous behaviour, start your terminal in the repository directory and type : `java -jar InteractiveAcademicUtility.jar`.
+
+Please create an _**issue**_ incase you find errors during running. The application requires internet connectivity, so make sure you are online while you start the application.
+
+## Grabbing the API Keys - Building from scratch
+
+The Application uses API from **NewsAPI.org**. So, will require one, its free.
+Also set up your **IBM Bluemix account** and create a text-to speech resource. Then get the credentials from there.
+
+Now, you need to create a **Keydata.java** file under directory `src/main/java/com/mycompany/jyotimaven`.
+
+Paste this piece of code inside **Keydata.java**:
+```
+package com.mycompany.jyotimaven;
+
+public class Keydata {
+
+  public static String NEWS_API_KEY = "";
+  public static String BLUEMIX_KEY = "";
+}
+```
+
+## Maven resources - Building from scratch
+
+In **pom.xml**, paste the following: 
+
+```
+<dependency>
+            <groupId>com.google.firebase</groupId>
+            <artifactId>firebase-admin</artifactId>
+            <version>6.5.0</version>
+</dependency>
+
+<dependency>
+            <groupId>com.android.support</groupId>
+            <artifactId>appcompat-v7</artifactId>
+            <version>27.1.1</version>
+            <scope>system</scope>
+            <systemPath>G:/Androidsdk/platform-tools/extras/android/m2repository/com/android/support/appcompat-v7/25.3.1/appcompat-v7-25.3.1-sources.jar</systemPath>
+</dependency>
+```
+
+Set the path accordingly with your _android sdk_. You may have to download the `platform-tools` if required.
+
+### Add Firebase to your app - Building from scratch
+
+We need to configure, firebase-admin sdk now. Head [here](https://github.com/firebase/firebase-admin-java) and [here](https://firebase.google.com/docs/admin/setup) to know how to do it.
+
+More simply, do as directed below :
+
+To use the Firebase Admin SDKs, you'll need a **Firebase project**, a **service account** to communicate with the Firebase service, and a **configuration file** with your _service account's credentials_.
+
+If you don't already have a Firebase project, add one in the **Firebase console**. The Add project dialog also gives you the option to add Firebase to an existing **Google Cloud Platform project**.
+
+Navigate to the **Service Accounts** tab in your **project's settings page**.
+
+Click the **Generate New Private Key** button at the bottom of the Firebase Admin SDK section of the Service Accounts tab.
+
+After you click the button, a **JSON file** containing your service account's credentials will be downloaded.
+
+## Final steps - Building from scratch
+
+You are all set to build the application. Incase, you are running **Netbeans**, do a `clean build` on the project. Now, the resources will get downloaded to your system.
+
+Once completed, you'll have a target folder in your source repository. Pull up the jar that says`...-with-dependencies to the root folder` and run it.
+
+You will need to create your own `audio/` and `images/` directory to provide some necessary resources now. 
---
+++ src/main/java/com/mycompany/jyotimaven/testtaker.java
@@ -0,0 +1,385 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jyotimaven;
+
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import net.thegreshams.firebase4j.error.FirebaseException;
+import net.thegreshams.firebase4j.error.JacksonUtilityException;
+import net.thegreshams.firebase4j.service.Firebase;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author Biswajit Roy
+ */
+public class testtaker extends javax.swing.JFrame {
+
+    /**
+     * Creates new form testtaker
+     */
+    
+    static int nextQues = 0, CorrectAns = 0;
+    QuizParser refinedData;
+    double averageValue;
+    public testtaker() {
+        try {
+            initComponents();
+            
+            String urlString = "https://jyotiserver.firebaseapp.com/quiz.json";
+            URL url = new URL(urlString);
+            URLConnection conn = url.openConnection();
+            InputStream is = conn.getInputStream();
+            // parsing file "sampleQuizData.json"
+            Object obj = new JSONParser().parse(new InputStreamReader(is));
+            quizProcessor(nextQues++, obj);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+
+    public void quizProcessor(int qno, Object obj){
+        //Setting user id...
+        userID.setText(UserData.email);
+        // typecasting obj to JSONObject
+        JSONObject jo = (JSONObject) obj;
+        // getting body array...
+        JSONArray bodyData = (JSONArray) jo.get("body");
+        System.out.println(bodyData.toArray()[qno]);
+        JSONObject result = (JSONObject) bodyData.toArray()[qno];
+        Gson gson = new Gson();
+        refinedData = gson.fromJson(result.toString(), QuizParser.class);
+        //Setting values according the quiz set...
+        option1.setText(refinedData.option1);
+        option2.setText(refinedData.option2);
+        option3.setText(refinedData.option3);
+        context.setText("<html>" + refinedData.title + "</html>");
+        
+        CompletableFuture.runAsync(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        WatsonBluemixReader.textToSpeech(refinedData.title);
+                        Thread.sleep(4000);
+                        WatsonBluemixReader.playAudioFeedback("audio\\instruction_options.wav");
+                        Thread.sleep(4000);
+                        WatsonBluemixReader.textToSpeech(refinedData.option1);
+                        WatsonBluemixReader.textToSpeech(refinedData.option2);
+                        WatsonBluemixReader.textToSpeech(refinedData.option3);
+                        Thread.sleep(2500);
+                        WatsonBluemixReader.playAudioFeedback("audio\\starting_service.wav");
+                        
+                        Runtime runtime = Runtime.getRuntime();
+                        Process proc = runtime.exec("powershell.exe python scripts\\fingerdetect.py");
+                        InputStream processStream = proc.getInputStream();
+                        InputStream errorStream = proc.getErrorStream();
+                        BufferedReader processBuffer = new BufferedReader(new InputStreamReader(processStream), 1);
+                            String line;
+                            int total = 0, eachValue, counter = 1;
+                            while ((line = processBuffer.readLine()) != null) {
+                                eachValue = Integer.parseInt(line);
+                                total = total + eachValue;
+                                counter++;
+                            }
+
+                            //Average value obtained by streaming gesture...
+                            double averageValue = (double)total/counter;
+
+                            int gestureResult = (int)Math.round(averageValue) + 1;
+                            processStream.close();
+                            processBuffer.close();
+                            
+                            if(gestureResult == 1){
+                                option1.setSelected(true);
+                                WatsonBluemixReader.playAudioFeedback("audio\\alert_result.wav");
+                                WatsonBluemixReader.textToSpeech(refinedData.option1);  
+                            }
+                            else if(gestureResult == 2) {
+                                option2.setSelected(true);
+                                WatsonBluemixReader.playAudioFeedback("audio\\alert_result.wav");
+                                WatsonBluemixReader.textToSpeech(refinedData.option2);
+                            }
+                            else if(gestureResult == 3) {
+                                option3.setSelected(true);
+                                WatsonBluemixReader.playAudioFeedback("audio\\alert_result.wav");
+                                WatsonBluemixReader.textToSpeech(refinedData.option3);
+                            }
+                            
+                            //record output if correct...
+                            if(option1.isSelected()==true && option1.getText().equals(refinedData.correct)){
+                                CorrectAns += 1;
+                            }
+                            else if(option2.isSelected()==true && option2.getText().equals(refinedData.correct)){
+                                CorrectAns += 1;
+                            }
+                            else if(option3.isSelected()==true && option3.getText().equals(refinedData.correct)){
+                                CorrectAns += 1;
+                            }
+                            else {
+                                WatsonBluemixReader.playAudioFeedback("audio\\error_result.wav");
+                            }
+                            
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+    }
+    
+    
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        title = new javax.swing.JLabel();
+        quit = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        labeluserID = new javax.swing.JLabel();
+        userID = new javax.swing.JLabel();
+        context = new javax.swing.JLabel();
+        nextquesbtn = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        option1 = new javax.swing.JRadioButton();
+        option2 = new javax.swing.JRadioButton();
+        option3 = new javax.swing.JRadioButton();
+        infoLabel = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Jyoti - Quizzing Service");
+        setResizable(false);
+
+        title.setFont(new java.awt.Font("HP Simplified Light", 0, 24)); // NOI18N
+        title.setForeground(new java.awt.Color(51, 51, 51));
+        title.setText(" Interactive academic utility - Quizera Platform");
+
+        quit.setText("Quit");
+        quit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                quitActionPerformed(evt);
+            }
+        });
+
+        labeluserID.setFont(new java.awt.Font("Tahoma", 0, 13)); // NOI18N
+        labeluserID.setForeground(new java.awt.Color(51, 51, 51));
+        labeluserID.setText("User ID:");
+
+        userID.setFont(new java.awt.Font("Tahoma", 0, 13)); // NOI18N
+        userID.setForeground(new java.awt.Color(102, 102, 102));
+
+        nextquesbtn.setText("Next Question");
+        nextquesbtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nextquesbtnActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(option1);
+        option1.setText("Option");
+
+        buttonGroup1.add(option2);
+        option2.setText("Option");
+
+        buttonGroup1.add(option3);
+        option3.setText("Option");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(option1)
+                    .addComponent(option2)
+                    .addComponent(option3))
+                .addContainerGap(207, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(option1)
+                .addGap(28, 28, 28)
+                .addComponent(option2)
+                .addGap(35, 35, 35)
+                .addComponent(option3)
+                .addContainerGap(17, Short.MAX_VALUE))
+        );
+
+        infoLabel.setForeground(new java.awt.Color(102, 102, 102));
+        infoLabel.setText("<html>*Note: You can quit the test any time by clicking on quit button. Your test will not be counted towards score. <br># [Next Question] button is experimental and can be replaced with a time limit</html>");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(labeluserID)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(userID, javax.swing.GroupLayout.PREFERRED_SIZE, 347, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(context, javax.swing.GroupLayout.PREFERRED_SIZE, 321, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(79, 79, 79)))
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(title)
+                        .addGap(63, 63, 63)
+                        .addComponent(quit))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(infoLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
+                        .addComponent(nextquesbtn)))
+                .addContainerGap())
+            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(quit)
+                    .addComponent(title))
+                .addGap(15, 15, 15)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(userID, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(labeluserID))
+                .addGap(67, 67, 67)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(context, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(nextquesbtn, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(infoLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void nextquesbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nextquesbtnActionPerformed
+        System.out.println(CorrectAns);
+        dispose();
+        if(nextQues<4){
+            new testtaker().setVisible(true);    
+        }
+            else{
+            try {
+                //Upload results...
+                JSONObject sessionData = new JSONObject();
+                sessionData.put("userid", UserData.email);
+                sessionData.put("score", CorrectAns+"");
+                
+                Firebase firebase = new Firebase("https://jyotiserver.firebaseio.com/");
+                System.out.println(firebase);
+                firebase.post(sessionData);
+                JOptionPane.showMessageDialog(null, "Thank you for taking the quiz, we will upload the results shortly !");
+                new scoreCard().setVisible(true);
+            } catch (FirebaseException ex) {
+                Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (JacksonUtilityException ex) {
+                Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(testtaker.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            }
+    }//GEN-LAST:event_nextquesbtnActionPerformed
+
+    private void quitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_quitActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_quitActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Windows look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Windows (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(testtaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(testtaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(testtaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(testtaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+//        testtaker ts = new testtaker();
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                if(nextQues<4)
+                   new testtaker().setVisible(true);
+            else{
+                JOptionPane.showMessageDialog(null, "Thank you for taking the quiz, we will upload the results shortly !");
+                new dashboard().setVisible(true);
+            }
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JLabel context;
+    private javax.swing.JLabel infoLabel;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel labeluserID;
+    private javax.swing.JButton nextquesbtn;
+    private javax.swing.JRadioButton option1;
+    private javax.swing.JRadioButton option2;
+    private javax.swing.JRadioButton option3;
+    private javax.swing.JButton quit;
+    private javax.swing.JLabel title;
+    private javax.swing.JLabel userID;
+    // End of variables declaration//GEN-END:variables
+}
